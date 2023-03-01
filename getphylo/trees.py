7901490c501e075b9c1c2f1fe4ab5828662a9e1a
DrBoothTJ <63703480+DrBoothTJ@users.noreply.github.com>
2023-03-01 15:34:30 +0100

    update trees

--- getphylo/trees.py
+++ getphylo/trees.py
@@ -7,12 +7,13 @@ Functions:
 '''
 import os
 import glob
-from getphylo import console, fasttree, io
+from getphylo.utils import io
+from getphylo.ext import fasttree
 
 def make_trees(output):
     '''main routine for trees'''
     io.make_folder(f'{output}/trees')
-    console.print_to_system("Building trees...")
+    io.print_to_system("Building trees...")
     for filename in glob.glob(f'{output}/aligned_fasta/*.fasta'):
         outfile = f'{output}/trees/{io.change_extension(filename, "tree").split("/")[2]}'
         fasttree.run_fasttree(filename, outfile)
