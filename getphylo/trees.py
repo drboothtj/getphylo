5f3fd7e1db4e3ab1f01e1bc3d413b809d2b7a560
DrBoothTJ <63703480+DrBoothTJ@users.noreply.github.com>
2023-03-02 15:06:44 +0100

    update trees

--- getphylo/trees.py
+++ getphylo/trees.py
@@ -2,18 +2,24 @@
 Build trees from a directory containing .fasta alignments
 
 Functions:
-
-
+    def make_trees(output: str) -> None
 '''
 import os
 import glob
+import logging
 from getphylo.utils import io
 from getphylo.ext import fasttree
 
-def make_trees(output):
-    '''main routine for trees'''
-    io.make_folder(f'{output}/trees')
-    io.print_to_system("Building trees...")
-    for filename in glob.glob(f'{output}/aligned_fasta/*.fasta'):
-        outfile = f'{output}/trees/{io.change_extension(filename, "tree").split("/")[2]}'
+def make_trees(output: str) -> None:
+    '''Main routine for trees.
+        Arguments:
+            output: path to the output directory
+        Returns:
+            None
+    '''
+    tree_directory = os.path.join(output, 'trees'
+    io.make_folder(tree_directory)
+    logging.info("Building trees...")
+    for filename in glob.glob(os.path.join(output, 'aligned_fasta/*.fasta'):
+        outfile = os.path.join(tree_directory, io.change_extension(filename, "tree"))
         fasttree.run_fasttree(filename, outfile)
