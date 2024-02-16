3fd3d088b15f62572c27906963ad8dd5e7d2e6b6
DrBoothTJ <booth.thom@gmail.com>
2024-02-16 16:18:53 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="getphylo",
-    version="0.2.0",
+    version="0.2.1",
     author="Thomas J. Booth",
     author_email="thoboo@biosustain.dtu.dk",
     packages=find_packages(),
